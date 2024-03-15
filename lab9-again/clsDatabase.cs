01667d14ea7bcd3f075c336f9f08f4f134d3af39
dezs <workingdezs@gmail.com>
2024-03-15 08:06:16 +0700

    first commit

---
+++ lab9-again/clsDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace lab9_again
+{
+    class clsDatabase
+    {
+        public static SqlConnection con;
+        public static bool openConnection()
+        {
+            try
+            {
+                con = new SqlConnection("Server=WinDev2311Eval; Database=lab9;User Id=mylogin;password=mylogin");
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool closeConnection()
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
